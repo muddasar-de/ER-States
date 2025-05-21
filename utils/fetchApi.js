0cc21fa5097f65324283661fec26f656011663a0
muddasar-de <muddasarajmal@gmail.com>
2025-05-21 22:01:42 +1000

    2nd commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': "539b35b7b0msh66c6bec0a075e30p1acf5ejsn42578244fe27" ,
+      'x-rapidapi-key': "539b35b7b0msh66c6bec0a075e30p1acf5ejsn42578244fe27",
     },
   });
     
