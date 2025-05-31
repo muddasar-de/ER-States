cf84e66fbf9eecfe358d0bbe8076ee269ebd1eca
muddasar-de <muddasarajmal@gmail.com>
2025-05-31 22:35:54 +1000

    setting config for images...

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images:{
-    domains:['https://comvision.pk/AsianPics/','comvision.pk','bayut-production.s3.eu-central-1.amazonaws.com',"picsum.photos"]
+    domains:['https://comvision.pk/AsianPics/','comvision.pk','bayut-production.s3.eu-central-1.amazonaws.com','picsum.photos','www.graana.com','images.graana.com','zameen.com','media.zameen.com']
+    
 },
 }
